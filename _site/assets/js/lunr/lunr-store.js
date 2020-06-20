02b6540de5fa1f3acc7190a9378d4362c87e7ac3
Ravi Solter <ravisolter@gmail.com>
2020-06-20 07:51:11 -0400

    changes to footer

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "Forecasting Capital Bikeshare usage with exponential smoothing",
+        "excerpt":"Forecasting Capital Bikeshare usage with Exponential Smoothing Background I’ve always loved biking. At age 18, I rode my bike 140 miles with a few friends one summer to Chicago, sleeping outside along the way. At age 29, I took a much less advised trip from DC to NYC on Dec,...","categories": ["R","forecasting"],
+        "tags": [],
+        "url": "http://localhost:4000/r/forecasting/Monthly_Bike_Forecast_ETS/",
+        "teaser": null
+      },{
+        "title": "Forecasting Capital Bikeshare usage with ARIMA",
+        "excerpt":"Forecasting Capital Bikeshare usage with ARIMA Having used DC Capital BikeShare data previously to forecast with exponential smoothing models, I wanted to do the same with ARIMA modeling. ARIMA a complementary method for forecasting univariate timeseries, but it also allows for explanatory variables in the regression. In this case, I’ve...","categories": ["R","forecasting"],
+        "tags": [],
+        "url": "http://localhost:4000/r/forecasting/Monthly_Bike_Forecast/",
+        "teaser": null
+      },{
+        "title": "Daily Forecasting of Capital Bikeshare usage with Facebook's prophet",
+        "excerpt":"Predicting Daily DC Bike Share Ridership with prophet The prophet package was created by Facebook in 2017 and provides a procedure for forecasting time series data. It’s a great package that’s meant to help produce reasonable forecasts with messy data without much manual effort. Prophet can handle, yearly, weekly, and...","categories": ["R","forecasting"],
+        "tags": [],
+        "url": "http://localhost:4000/r/forecasting/Daily_Bike_Forecast/",
+        "teaser": null
+      },{
+        "title": "How I built this portfolio",
+        "excerpt":"How I made this This portfolio was built using Jekyll, a static website generator that is a popular choice among users who wish to build a personal or portfolio website. I chose it because it is simple to set up if you have little to no experience in web development...","categories": ["Jekyll","GitHub","reference"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/github/reference/HowIMadeThis/",
+        "teaser": null
+      },{
+        "title": "Data Science on Chrome OS + Linux",
+        "excerpt":"Data Science on Chrome OS + Linux ChromeOS is a linux-based operating system developed by Google and is the default OS on all Chromebooks. It’s a very lightweight OS and was initially limited Chrome and the suite of Google office applications. However, overtime it has grown to support Android Apps...","categories": ["Linux","GitHub","R","python","reference"],
+        "tags": [],
+        "url": "http://localhost:4000/linux/github/r/python/reference/DSonChromeOS/",
+        "teaser": null
+      },{
+        "title": "Predicting Soccer Match Outcomes using caret",
+        "excerpt":"Predicting Soccer Match Outcomes using caret 1. Objective The goal of this post is to try and predict the outcome of soccer matches in the Italian top-flight division, Serie A. There are three possible outcomes for each match: a home win, an away win, or a draw. To make things...","categories": ["R","caret"],
+        "tags": [],
+        "url": "http://localhost:4000/r/caret/SerieA_Classification/",
+        "teaser": null
+      },{
+        "title": "pandas Overview",
+        "excerpt":"pandas Overview Making the move from R to python, I feel out of place without my familiar tidyverse of packages for data maniuplation and visualization. As such, I’ve been spending a lot of time learning pandas, the most popular data analysis and manipulation tool in python. This post is meant...","categories": ["python","pandas","reference"],
+        "tags": [],
+        "url": "http://localhost:4000/python/pandas/reference/Pandas-Overview/",
+        "teaser": null
+      }]
